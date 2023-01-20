d494320768ff41a5559f66599dd1ecc92e42f55b
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-01-21 02:48:21 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BlackCoderRoleBased.Models;
+
+namespace BlackCoderRoleBased.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[AllowAnonymous]
+
+        [Authorize(Roles = "dummy_2")] // access
+        //[Authorize(Roles = "dummy_3")] // no access
+
+        // OR: role dummy_2 OR dummy_3 needed
+        //[Authorize(Roles = "dummy_2,dummy_3")] // OR
+
+        // AND: if on separate line: role dummy_2 AND dummy_3 needed
+        //[Authorize(Roles = "dummy_2")]
+        //[Authorize(Roles = "dummy_3")]
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
